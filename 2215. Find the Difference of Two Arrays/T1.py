9ab2009f50371d7438de43ca6563ec88b3c32442
Tanmay Agarwal <95555693+TanmayAgarwal123@users.noreply.github.com>
2026-08-29 00:51:11 +0530

    delete

--- 2215. Find the Difference of Two Arrays/T1.py
+++
@@ -1,4 +0,0 @@
-class Solution:
-    def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
-        ans=[list(set(nums1)-set(nums2)),list(set(nums2)-set(nums1))]
-        return ans
